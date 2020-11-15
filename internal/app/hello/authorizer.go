a39376e68f66c9aaf83e016d4312cdf74a627e0e
rafael.dasilvacosta <rafael.dasilvacosta@rakuten.com>
2020-11-16 00:04:37 +0900

    Abstracting authorizer middleware so that it takes a function

--- internal/app/hello/authorizer.go
+++ internal/app/hello/authorizer.go
@@ -4,20 +4,12 @@ import (
 	"net/http"
 )
 
-func (s *Server) checkAuthorization(h http.HandlerFunc) http.HandlerFunc {
+func (s *Server) CheckAuthorization(authFunc func(r *http.Request) bool, handle http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if isAuthorized(r) {
-			h(w, r)
+		if authFunc(r) {
+			handle(w, r)
 		} else {
 			s.respond(w, r, struct{}{}, http.StatusUnauthorized)
 		}
 	}
-}
-
-func isAuthorized(r *http.Request) bool {
-	auth, password, ok := r.BasicAuth()
-	if !ok {
-		return false
-	}
-	return auth == "admin" && password == "password"
 }
