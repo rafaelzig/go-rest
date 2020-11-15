c6e9bdeb1f540ce0c197622612aa1beda8608cce
rafael.dasilvacosta <rafael.dasilvacosta@rakuten.com>
2020-11-15 23:14:02 +0900

    add integration test for server.go

---
+++ test/internal/app/hello/server_test.go
@@ -0,0 +1,30 @@
+package hello
+
+import (
+	"encoding/json"
+	"github.com/matryer/is"
+	"github.com/rafaelzig/go-rest/internal/app/hello"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServerIntegration(t *testing.T) {
+	is := is.New(t)
+	srv := httptest.NewServer(hello.NewServer(nil))
+	defer srv.Close()
+	resp, err := http.Get(srv.URL + "/health")
+	is.NoErr(err)
+	is.Equal(resp.StatusCode, http.StatusOK)
+	is.Equal(resp.Header.Get("Content-Type"), "application/json")
+	type response = struct {
+		Status string `json:"status"`
+	}
+	expected := response{
+		Status: "ready",
+	}
+	var actual response
+	err = json.NewDecoder(resp.Body).Decode(&actual)
+	is.NoErr(err)
+	is.Equal(actual, expected)
+}
