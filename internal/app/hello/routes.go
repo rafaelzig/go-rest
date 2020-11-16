cbab489693ff4b7c239fb3a9eaba4369e69f5daf
rafael.dasilvacosta <rafael.dasilvacosta@rakuten.com>
2020-11-16 23:05:15 +0900

    Indenting routes.go

--- internal/app/hello/routes.go
+++ internal/app/hello/routes.go
@@ -3,7 +3,10 @@ package hello
 import "net/http"
 
 func (s *Server) initRoutes() {
-	s.router.HandleFunc("/", s.Log(s.handleIndex())).Methods(http.MethodGet)
-	s.router.HandleFunc("/health", s.Log(s.handleHealth())).Methods(http.MethodGet)
-	s.router.HandleFunc("/shutdown", s.Log(s.authorize(s.basicAuth("admin", "password"), s.handleShutdown()))).Methods(http.MethodDelete)
+	s.router.HandleFunc("/", s.Log(
+		s.handleIndex())).Methods(http.MethodGet)
+	s.router.HandleFunc("/health", s.Log(
+		s.handleHealth())).Methods(http.MethodGet)
+	s.router.HandleFunc("/shutdown", s.Log(s.authorize(s.basicAuth("admin", "password"),
+		s.handleShutdown()))).Methods(http.MethodDelete)
 }
