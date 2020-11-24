b3a4fb4c2633c8b8eb0c3afe9e7ca5ae2876dcb5
rafael.dasilvacosta <rafael.dasilvacosta@rakuten.com>
2020-11-24 12:54:01 +0900

    refactoring code to simplify readability

--- cmd/hello/hello.go
+++ cmd/hello/hello.go
@@ -19,25 +19,13 @@ const defaultServerPort = uint16(8080)
 func main() {
 	h := createHandler()
 	srv := createServer(h)
-	idleConnsClosed := make(chan struct{})
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, syscall.SIGTERM, syscall.SIGKILL)
-		<-sigint
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		h.Info.Println("Initiating HTTP server Shutdown")
-		if err := srv.Shutdown(ctx); err != nil {
-			// Error from closing listeners, or context timeout:
-			h.Info.Printf("HTTP server Shutdown: %v", err)
-		}
-		close(idleConnsClosed)
-	}()
+	shutdownChan := make(chan struct{})
+	go handleGracefulShutdown(h, srv, shutdownChan)
 	h.Info.Printf("Starting HTTP server on http://localhost%s\n", srv.Addr)
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		h.Error.Fatalf("HTTP server ListenAndServe: %v", err)
 	}
-	<-idleConnsClosed
+	<-shutdownChan
 	h.Info.Print("HTTP server gracefully Shutdown")
 }
 
@@ -59,6 +47,20 @@ func createServer(h *hello.Server) *http.Server {
 		Handler: h,
 	}
 	return server
+}
+
+func handleGracefulShutdown(h *hello.Server, srv *http.Server, shutdownChan chan struct{}) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, syscall.SIGTERM, syscall.SIGKILL)
+	<-interruptChan
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	h.Info.Println("Initiating HTTP server Shutdown")
+	if err := srv.Shutdown(ctx); err != nil {
+		// Error from closing listeners, or context timeout:
+		h.Info.Printf("HTTP server Shutdown: %v", err)
+	}
+	close(shutdownChan)
 }
 
 func parseServerPort(str string) uint16 {
